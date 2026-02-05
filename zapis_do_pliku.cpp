1864b21e9dd40b9b25ee2446bbd5bc48ee436b98
wojtpit99-hub <wojtpit99@gmail.com>
2026-02-05 22:12:13 +0100

    Update zapis_do_pliku.cpp

--- zapis_do_pliku.cpp
+++ zapis_do_pliku.cpp
@@ -1,24 +1,25 @@
 #include <iostream>
 #include <fstream>
-
 using namespace std;
 
 int main()
 {
-    ofstream file("data.txt", ios::app);
-
-    if (file.is_open())
-        {
-        file << "Mam" << endl;
-        file << "nadzieje, " << endl;
-        file << "ze zdalem" << endl;
-
+    ofstream file ("plik.txt", ios::app);
+    
+    if(file.is_open())
+    {
+        file << "test" << endl;
+        file << "test2" << endl;
+        file << "test3" << endl;
+        
         file.close();
-        std::cout << "Zapisano pomyslnie." << std::endl;
-    } else {
-        std::cout << "Blad podczas otwierania pliku." << std::endl;
+        cout << "Udalo sie zapisac plik.";
     }
 
+    else
+    {
+        cout << "Wystapil blad podczas otwierania pliku.";
+    }
+    
     return 0;
 }
-
