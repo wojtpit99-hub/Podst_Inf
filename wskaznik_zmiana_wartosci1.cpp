6ea845375ec3847c2bb497b78742569618286d5c
wojtpit99-hub <wojtpit99@gmail.com>
2026-02-05 22:05:53 +0100

    Update wskaznik_zmiana_wartosci1.cpp

--- wskaznik_zmiana_wartosci1.cpp
+++ wskaznik_zmiana_wartosci1.cpp
@@ -1,24 +1,22 @@
 #include <iostream>
-
 using namespace std;
 
 int main()
 {
-    int a = 10;
-    int b = 20;
+    int a = 5;
+    int b = 15;
 
-    int* wsk_a = &a; // wskaŸnik na zmienn¹ a
-    int* wsk_b = &b; // wskaŸnik na zmienn¹ b
+    int* wskaznik_a = &a;
+    int* wskaznik_b = &b;
 
-    cout << "Przed zmiana:" << endl;
+    cout << "Poczatkowe wartosci zmiennych: " << endl;
     cout << "a = " << a << ", b = " << b << endl;
 
-    *wsk_a = 30; // zmiana wartoœci zmiennej a przez wskaŸnik
-    *wsk_b = 40; // zmiana wartoœci zmiennej b przez wskaŸnik
+    *wskaznik_a = 100;
+    *wskaznik_b = -100;
 
-    cout << "Po zmianie:" << endl;
+    cout << "Zmienione wartosci zmienntch: " << endl;
     cout << "a = " << a << ", b = " << b << endl;
 
     return 0;
 }
-
