11c93de6326d4a705bc5d69e932254af7e751651
wojtpit99-hub <wojtpit99@gmail.com>
2026-02-05 20:46:00 +0100

    Update 3_konstruktory.cpp

--- 3_konstruktory.cpp
+++ 3_konstruktory.cpp
@@ -1,36 +1,41 @@
 #include <iostream>
-
 using namespace std;
-class egzamin
+
+class Obiekt
 {
+private:
+    int x;
+    int y;
 public:
-    int x, y;
     // konstruktor bezargumentowy
-    egzamin()
+    Obiekt()
     {
-     cout << "Konstruktor bezargumentowy zostal wywolany" <<endl;
+        cout << "Wywolales konstrukor bezargumentowy" << endl;
     }
+
     // konstruktor z jednym argumentem
-    egzamin(int value)
+    Obiekt(int wartosc)
     {
-        x = value;
-        y = value;
+        x = wartosc;
+        y = wartosc; 
+        
+        cout << "Wywolales konstruktor jednoargumentowy " << "Wartosc x: " << x << " Wartosc y: " << y << endl;
     }
     // konstruktor z dwoma argumentami
-    egzamin(int x_value, int y_value)
+    Obiekt(int wartosc_x, int wartosc_y)
     {
-        x = x_value;
-        y = y_value;
+        x = wartosc_x;
+        y = wartosc_y;
+
+        cout << "Wywolales konstruktor dwuargumentowy " << "Wartosc x: " << x << " Wartosc y: " << y << endl;
     }
 };
+
 int main()
 {
-    egzamin obj1;            // u¿ycie konstruktora bezargumentowego
-    egzamin obj2(5);         // u¿ycie konstruktora z jednym argumentem
-    egzamin obj3(2, 4);      // u¿ycie konstruktora z dwoma argumentami
-    //cout << "obj1: x=" << obj1.x << ", y=" << obj1.y << endl;
-    cout << "obj2: x=" << obj2.x << ", y=" << obj2.y << endl;
-    cout << "obj3: x=" << obj3.x << ", y=" << obj3.y << endl;
+    Obiekt obj0;
+    Obiekt obj1(5);
+    Obiekt obj2(10, 5);
 
     return 0;
 }
